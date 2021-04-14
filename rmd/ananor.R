3970416aa7b9979c6ef8accbb23a648815b977b6
marparven1 <marparven1@alum.us.es>
2021-04-14 09:33:51 +0200

    Aubo hoja ananaor

--- rmd/ananor.R
+++ rmd/ananor.R
@@ -17,4 +17,47 @@ ananor<-function(x)
 }
 
 
+ananor_tidy<-function(x)
+{
+        n<-length(x)
+        datos = data.frame(X = x)
+        p1 = ggplot(datos, aes(sample = X)) +
+                geom_qq() +
+                geom_qq_line() +
+                labs(
+                        title = paste("Gráfico Normal de Prob. n=",n)
+                )
+        p2 = ggplot(datos, aes(x = X)) +
+                geom_boxplot() +
+                labs(
+                        title = "Gráfico de caja y bigotes"
+                )
+        p3 = ggplot(datos, aes(x = X)) +
+                #geom_histogram(aes(y = ..density..), fill = "red",col="black", bins = 30) +
+                geom_density(col="red") +
+                xlim(min(datos$X),max(datos$X)) +
+                stat_function(aes(x=seq(min(X),max(X),length = length(X))),
+                              fun = dnorm, args = list(mean = mean(datos$X),
+                                                       sd = sd(datos$X)),color = "blue") +
+                labs(
+                        title = "Función de densidad"
+                )
+        p4 = ggplot(datos, aes(x = X)) +
+                stat_ecdf(geom = "step") +
+                xlim(min(datos$X),max(datos$X)) +
+                stat_function(aes(x=seq(min(X),max(X),length = length(X))),
+                              fun = pnorm, args = list(mean = mean(datos$X),
+                                                       sd = sd(datos$X)),color = "blue") +
+                labs(
+                        title = "Función de distribución"
+                )
+        print(shapiro.test(x))
+        
+        (p1 | p2) / (p3 | p4)
+        
+}
+
+
+
+
 
